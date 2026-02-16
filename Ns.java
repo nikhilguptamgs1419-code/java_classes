76092393f8076a2dd1b3c1e0efe05f081c2bfda2
Nikhil Gupta <nikhilguptamgs1419@gmail.com>
2026-02-16 11:45:08 +0530

    16th_feb_2026

---
+++ Ns.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Ns {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            System.out.println(i);
+        }
+    }
+        
+    
+}
+
