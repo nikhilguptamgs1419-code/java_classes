7324ee348c4b365024aec3f0a71a5fa4184df86c
Nikhil Gupta <nikhilguptamgs1419@gmail.com>
2026-03-16 14:46:11 +0530

    Array

---
+++ Arrayinput.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Arrayinput {
+    
+public static void main(String args[])
+{
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of array;");
+    int size = sc.nextInt();
+    int arr[] = new int [size];
+    for(int i=0;i<size;i++)
+    {
+        arr[i]=sc.nextInt();
+
+    }
+    System.out.println("Output of array");
+for(int i=0;i<size;i++)
+{
+    System.out.println(arr[i]);
+}
+}
+}
